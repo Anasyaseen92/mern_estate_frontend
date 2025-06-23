59c3981913ffaec732ceaabe4e2df981207343e4
Anasyaseen92 <anasy7148@gmail.com>
2025-06-23 10:35:46 -0700

    oo

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/", 
+  base: "/",
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-estate-backend-pied.vercel.app/",
+        target: "https://mern-estate-backend-pied.vercel.app",
         changeOrigin: true,
         secure: false,
-         rewrite: (path) => path.replace(/^\/api/, ''),
+        // ❌ Don't strip the /api prefix
       },
     },
   },
-
   plugins: [react(), tailwindcss()],
 });
